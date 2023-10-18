1032be3e111d99b08175ac67414f878fb958763b
Sherlock-Homles <sherlockholmes2329@gmail.com>
2023-10-18 17:23:56 +0800

    修改main文件

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 // 引入我们导出的 router
-import { setupRouter } from '@/router'
+import { setupRouter } from "@/router";
 
 const setupApp = async () => {
-  const app = createApp(App)
+  const app = createApp(App);
   // 创建路由
-  setupRouter(app)
-  app.mount('#app')
-}
+  setupRouter(app);
+  app.mount("#app");
+};
 
-setupApp()
+setupApp();
