e79e0efd0ead1ddd49b5224caea2e1cceba1485c
Sherlock-Homles <sherlockholmes2329@gmail.com>
2023-10-18 17:21:54 +0800

    路由文件修改

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,24 @@
-import type { App } from 'vue'
-import ServerRouter from './modules/router'
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import type { App } from "vue";
+import ServerRouter from "./modules/router";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
-export const publicRoutes: Array<RouteRecordRaw> = [...ServerRouter]
+export const publicRoutes: Array<RouteRecordRaw> = [...ServerRouter];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: publicRoutes,
-})
+});
 
 /* 初始化路由表 */
 export function resetRouter() {
   router.getRoutes().forEach((route) => {
-    const { name } = route
+    const { name } = route;
     if (name) {
-      router.hasRoute(name) && router.removeRoute(name)
+      router.hasRoute(name) && router.removeRoute(name);
     }
-  })
+  });
 }
 /* 导出 setupRouter */
 export const setupRouter = (app: App<Element>) => {
-  app.use(router)
-}
+  app.use(router);
+};
--- src/router/modules/router.ts
+++ src/router/modules/router.ts
@@ -1,46 +1,46 @@
-import AppLayout from '@/layout/AppLayout.vue'
+import AppLayout from "@/layout/AppLayout.vue";
 export default [
   {
-    path: '/',
-    name: 'homeIndex',
-    component: () => import('@/views/home/homeIndex.vue'),
+    path: "/",
+    name: "homeIndex",
+    component: () => import("@/views/home/homeIndex.vue"),
     meta: {
-      title: '首页',
+      title: "首页",
     },
   },
   {
-    path: '/login',
-    name: 'loginIndex',
-    component: () => import('@/views/login/loginIndex.vue'),
+    path: "/login",
+    name: "loginIndex",
+    component: () => import("@/views/login/loginIndex.vue"),
     meta: {
-      title: '登录页',
+      title: "登录页",
     },
   },
   {
-    path: '/layout',
-    name: 'layoutIndex',
+    path: "/layout",
+    name: "layoutIndex",
     component: AppLayout,
     children: [
       {
-        path: '/admin',
-        name: 'adminIndex',
-        component: () => import('@/views/admin/adminIndex.vue'),
+        path: "/admin",
+        name: "adminIndex",
+        component: () => import("@/views/admin/adminIndex.vue"),
         meta: {
           isShow: true, // 控制当前项是否在菜单栏中渲染出来，比如你写了 login 页面的路由，但是并不希望 login在menu菜单中渲染出来，即可设为false
-          title: '后台', // menu菜单项的名称，没啥好说的
-          icon: 'menu-home', // menu菜单项的图标，我此处是与封装好的 svg 组件结合使用的
+          title: "后台", // menu菜单项的名称，没啥好说的
+          icon: "menu-home", // menu菜单项的图标，我此处是与封装好的 svg 组件结合使用的
         },
       },
       {
-        path: '/adminFirst',
-        name: 'adminFirst',
-        component: () => import('@/views/admin/adminFirst.vue'),
+        path: "/adminFirst",
+        name: "adminFirst",
+        component: () => import("@/views/admin/adminFirst.vue"),
         meta: {
           isShow: true,
-          title: '后台一',
-          icon: 'menu-home',
+          title: "后台一",
+          icon: "menu-home",
         },
       },
     ],
   },
-]
+];
